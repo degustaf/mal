fdf64c53fb6bb281bd2988e9303ca1e101f3a532
Derek Gustafson <degustaf@gmail.com>
2025-11-29 18:40:07 -0500

    Add initial do nothing repl

--- src/mal.cpp
+++ src/mal.cpp
@@ -1,3 +1,25 @@
 #include "mal.hpp"
 
-int main(void) { return 0; }
+#include <iostream>
+#include <string>
+
+static std::string read(std::string str) { return str; }
+
+static std::string eval(std::string ast, void *) { return ast; }
+
+static std::string print(std::string exp) { return exp; }
+
+static std::string rep(std::string str) {
+  return print(eval(read(str), nullptr));
+}
+
+int main(void) {
+  std::string str;
+
+  std::cout << "user> ";
+
+  while (std::getline(std::cin, str)) {
+    std::cout << rep(str) << "\n";
+    std::cout << "user> ";
+  }
+}
